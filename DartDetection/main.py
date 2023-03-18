30839b7b27b71f77ef312a054b0502a593387382
Andres Tangarife <andresfelta95@gmail.com>
2023-03-18 13:20:12 -0600

    debugging

--- DartDetection/main.py
+++ DartDetection/main.py
@@ -79,89 +79,36 @@ sensors = [sensor1, sensor2, sensor3, sensor4, sensor5, sensor6, sensor7, sensor
 player1_points = 0
 player2_points = 0
 
+""" This function will read the disance from the sensor and return the distance in cm
+    it will read the distance 100 times and return the average but ignore the first 10 readings
+    Parameters: echo - the echo pin of the sensor
+    Returns: distance in cm
+"""
 def read_distance(echo):
-    #   Send a 10us pulse to the trigger pin
-    trig.value(0)
-    time.sleep_us(2)
-    trig.value(1)
-    time.sleep_us(TRIG_PULSE_DURATION_US)
-    trig.value(0)
-
-    #   Wait for the echo pin to go high
-    while echo.value() == 0:
-        pass
-    start_time = time.ticks_us()
-
-    #   Wait for the echo pin to go low
-    while echo.value() == 1:
-        pass
-    end_time = time.ticks_us()
-    #   Calculate the distance
-    duration = time.ticks_diff(end_time, start_time)
-    distance = (duration * Sound_SPEED) / 20000
-
-    return distance
+    distance_cm = 0
+    for i in range(10):
+        trig.value(0)
+        time.sleep_us(5)
+        trig.value(1)
+        time.sleep_us(TRIG_PULSE_DURATION_US)
+        trig.value(0)
+        ultrason_duration = time_pulse_us(echo, 1, 30000)
+        if i > 0:
+            distance_cm += Sound_SPEED * ultrason_duration /20000
+    distance_cm = distance_cm / 9
+    #rounding the distance to no decimal places
+    distance_cm = math.floor(distance_cm)
+    return distance_cm   
 
     
 
 while True:
     #   Read the distance from each sensor
-    mux.set_channel(0)
-    distance1 = read_distance(sensor1)
-    time.sleep(0.2)
-    mux.set_channel(1)
-    distance2 = read_distance(sensor2)
-    time.sleep(0.2)
-    mux.set_channel(2)
-    distance3 = read_distance(sensor3)
-    time.sleep(1)
-    mux.set_channel(3)
-    distance4 = read_distance(sensor4)
-    time.sleep(1)
-    mux.set_channel(4)
-    distance5 = read_distance(sensor5)
-    time.sleep(1)
-    mux.set_channel(5)
-    distance6 = read_distance(sensor6)
-    time.sleep(1)
-    mux.set_channel(6)
-    distance7 = read_distance(sensor7)
-    time.sleep(1)
-    mux.set_channel(7)
-    distance8 = read_distance(sensor8)
-    time.sleep(1)
-    mux.set_channel(8)
-    distance9 = read_distance(sensor9)
-    time.sleep(1)
-    mux.set_channel(9)
-    distance10 = read_distance(sensor10)
-    time.sleep(1)
-
-    #    Print the distance
-    print(f"Distance Sensor 1: {distance1} cm")
-    print(f"Distance Sensor 2: {distance2} cm")
-    print(f"Distance Sensor 3: {distance3} cm")
-    print(f"Distance Sensor 4: {distance4} cm")
-    print(f"Distance Sensor 5: {distance5} cm")
-    print(f"Distance Sensor 6: {distance6} cm")
-    print(f"Distance Sensor 7: {distance7} cm")
-    print(f"Distance Sensor 8: {distance8} cm")
-    print(f"Distance Sensor 9: {distance9} cm")
-    print(f"Distance Sensor 10: {distance10} cm")
-
-    
-    # for i in range(10):
-    #     mux.set_channel(i)
-    #     trig.value(0)
-    #     time.sleep_us(5)
-    #     trig.value(1)
-    #     time.sleep_us(TRIG_PULSE_DURATION_US)
-    #     trig.value(0)
-    #     ultrason_duration = time_pulse_us(sensors[i], 1, 30000)
-    #     distance_cm = Sound_SPEED * ultrason_duration /20000
-    #     print(f"Distance Sensor {i + 1} : {distance_cm} cm")
-    #     time.sleep_ms(500) 
-    #     time.sleep(1)
+    for i in range(10):
+        mux.set_channel(i)
+        distance = read_distance(sensors[i])
+        print("Sensor: ", i + 1, " Distance: ", distance)
+        time.sleep(1)
 
 def findingTheDart(dartList):
     position = 0
